5ee40b7b767a1414c16713b692a5deb62db3cebb
Ubuntu <ubuntu@ip-172-31-91-250.ec2.internal>
2023-10-10 22:02:15 +0000

    change development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+   config.hosts << "dec68d072dfd4d849233a725b99cfb1b.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
